9451c2559af07e013a173f81d1287c449f3c3599
fernandocrmeneses <fernandocrm_2@outlook.com>
2024-04-02 19:44:47 -0300

    implementando a camada de dados

--- app/data/Split.py
+++ app/data/Split.py
@@ -1,6 +1,12 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
 class Split:
     def __init__(self) -> None:
         pass
-    
-    def __EfetuaSplitTexto(self,textoDocumento):
+
+    #metodo efetua o split do documento para enviar ao vectorDB.
+    def EfetuaDivisaoDocumento(self, textoDocumento):
         self.textoDocumento = textoDocumento
+        recursivo = RecursiveCharacterTextSplitter(textoDocumento)
+        return recursivo
+    
--- app/data/VectorDataBase.py
+++ app/data/VectorDataBase.py
@@ -1,3 +1,12 @@
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import DocArrayInMemorySearch
+
 class VectorDataBase:
     def __init__(self) -> None:
         pass
+
+    def CarregaDocumentosVectorStore(self, documentoDividido):
+        self.documentosDivididos = documentoDividido
+        vectorStore = DocArrayInMemorySearch.from_documents(
+            documentoDividido, embedding = OpenAIEmbeddings()
+        )
