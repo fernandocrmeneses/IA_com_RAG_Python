c12fc81c735806f6d4d27588529816c039589217
fernandocrmeneses <fernandocrm_2@outlook.com>
2024-04-03 09:15:18 -0300

    implementando a camada de dados

--- app/data/VectorDataBase.py
+++ app/data/VectorDataBase.py
@@ -2,11 +2,14 @@ from langchain_openai.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores import DocArrayInMemorySearch
 
 class VectorDataBase:
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        vectorStore = None
 
+    #Carrega os documentos que foram feitos o Split(divisão), no banco de dados de vetor.
     def CarregaDocumentosVectorStore(self, documentoDividido):
         self.documentosDivididos = documentoDividido
         vectorStore = DocArrayInMemorySearch.from_documents(
             documentoDividido, embedding = OpenAIEmbeddings()
         )
+        return vectorStore
+       
